327cbfb131bcb8563c4199babef148155755ee17
EnesKilicaslan <eeneskilicaslan@gmail.com>
2021-08-26 08:45:59 -0700

    Add icons, create their components and show them in storybook

---
+++ stories/Icons.stories.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  ArrowBottom,
+  BookmarkFill,
+  Bookmark,
+  Emoji,
+  Explore,
+  ExplorerFill,
+  Gif,
+  HomeFill,
+  Home,
+  Like,
+  ListsFill,
+  Lists,
+  Media,
+  MessagesFill,
+  Messages,
+  More,
+  NotificationFill,
+  Notification,
+  Options,
+  ProfileFill,
+  Profile,
+  Question,
+  Reply,
+  Retweet,
+  Search,
+  Share,
+  TimelineProp,
+  Twitter,
+} from "../components/icons";
+
+export default {
+  title: "Component/Icons",
+  argTypes: {},
+};
+
+const Template = (args) => {
+  return (
+    <div className="icons">
+      <ArrowBottom />
+      <BookmarkFill />
+      <Bookmark />
+      <Emoji />
+      <Explore />
+      <ExplorerFill />
+      <Gif />
+      <HomeFill />
+      <Home />
+      <Like />
+      <ListsFill />
+      <Lists />
+      <Media />
+      <MessagesFill />
+      <Messages />
+      <More />
+      <NotificationFill />
+      <Notification />
+      <Options />
+      <ProfileFill />
+      <Profile />
+      <Question />
+      <Reply />
+      <Retweet />
+      <Search />
+      <Share />
+      <TimelineProp />
+      <Twitter />
+    </div>
+  );
+};
+
+export const Icons = Template.bind({});
+Icons.args = {};
