5078bade4c65bc7199e6e245deda7fb05a86a7d3
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-04 03:03:29 -0700

    fix pipeline

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 export function imageLoader({ src }) {
   if (typeof src === "string") {
     return src;
   } else {
-    return `/images/${src}`;
+    const imgSrc = `/images/${src}`;
+    return isProd ? "/twitter-clone" + imgSrc : imgSrc;
   }
 }
