2fba894ca6eafb790ffa99962c9a9a3922877435
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-04 14:03:01 -0700

    fix image source

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,8 @@ export function imageLoader({ src }) {
   if (typeof src === "string") {
     return src;
   } else {
-    const imgSrc = `/images/${src}`;
-    return "/twitter-clone" + imgSrc;
+    // const imgSrc = `twitter-clone/images/${src}`;
+    // return "/twitter-clone" + imgSrc;
+    return `twitter-clone/images/${src}`;
   }
 }
