3e9373cd8bda40e1d7234bed639605eb37b9e7ed
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-04 00:57:03 -0700

    move images to public folder - fix

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./Avatar.module.css";
-import AvatarPhoto from "/images/avatar-default.png";
+import AvatarPhoto from "../public/images/avatar-default.png";
 
 export const Avatar = ({ src, alt, size = 47 }) => {
   const [error, setError] = useState(false);
