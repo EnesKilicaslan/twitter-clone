ae49d47fb25e5c47127f3e39be2a312f9b9337ac
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-06 13:33:53 -0700

    Staticly generate tweets

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Layout, Tweet, Loading } from "../components";
-import useSWR from "swr";
-import Fetcher from "../libs/Fetcher";
+import { data } from "./db/tweets";
 
 export const HomePage = ({ data }) => {
   // const { data } = useSWR("/api/tweet", Fetcher);
@@ -18,10 +17,9 @@ export const HomePage = ({ data }) => {
 };
 
 export async function getStaticProps(context) {
-  const res = await fetch("/api/tweet");
-  const data = await res.json();
-
   // return data
+
+  // const data = [];
 
   return {
     props: { data }, // will be passed to the page component as props
